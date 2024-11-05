6286c6ec330ec56603ddf5cc7aa1d6325b38fd66
kesh25 <kinyua.dn@outlook.com>
2024-11-05 11:34:30 +0300

    to desktop

--- src/app/books/[bookId]/page.tsx
+++ src/app/books/[bookId]/page.tsx
@@ -2,10 +2,13 @@ import Container from "@/components/container";
  
 
 
-export default function Page() {
+export default function Page({params}: {params: {bookId: string}}) {
 
     return (
-        <Container title="Book " subtitle="Processing">
+        <Container 
+            title="Book " 
+            subtitle="Processing"
+        >
              
         </Container>
     )
